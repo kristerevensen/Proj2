ba9f0f04ff78fe122517b0ed3b43545a2f12789e
Krister Ross <krister.ross.evensen@gmail.com>
2023-03-05 02:02:24 +0100

    added router

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -150,7 +150,7 @@ export default function Example() {
     </div>
     <div className="relative hidden w-0 flex-1 lg:block">
      <img
-      className="absolute inset-0 h-screen w-full object-cover"
+      className="absolute inset-0 h-fit w-full object-cover"
       src="https://images.unsplash.com/photo-1505904267569-f02eaeb45a4c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1908&q=80"
       alt=""
      />
