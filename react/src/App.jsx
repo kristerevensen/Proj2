7e898b3c39bab8aea137684be00a1ffc9771fc0d
Krister Ross <krister.ross.evensen@gmail.com>
2023-03-05 01:31:53 +0100

    added react

---
+++ react/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { BeakerIcon } from '@heroicons/react/24/solid'
+
+function App() {
+
+ return (
+  <div className="bg-red-500">
+   App.jsx
+   <BeakerIcon className="h-6 w-6 text-blue-500"/>
+  </div>
+ )
+}
+
+export default App
